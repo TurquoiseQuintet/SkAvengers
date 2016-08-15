e6811aafa1e910d350bacd93bccf796bae400a6c
Jamie <lanae_j@operamail.com>
2016-08-15 16:06:13 -0600

    route

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,28 +1,21 @@
 'use strict';
 var router = require('express').Router();
+var serverController=require('../controllers/serverController');
 
 router.post('/', function(req, res, next){
   //as an admin, create a new task for the current hunt
 });
 
-router.get('/:task_id', function(req, res, next){
-  //view a specific task
-});
+router.get('/:task_id', serverController.gettask);
 
-router.post('/user/:_task_id', function(req, res, next){
-  //submit task to be reviewed by the admin
-});
+router.post('/user/:_task_id', );
 
-router.post('/admin/:task_id', function(req, res, next){
+router.post('/admin/:task_id', serverController.posttask){
   //admin submit task as complete or not
 });
 
-router.delete('/:task_id', function(req, res, next){
-  //as an admin, delete specific task
-})
+router.delete('//:task_id', serverController.deletetask);
 
-router.put('/:task_id', function(req, res, next){
-  //as an admin, edit specific task
-});
+router.put('/admin/:task_id', serverController.edittask);
 
 module.exports = router;
