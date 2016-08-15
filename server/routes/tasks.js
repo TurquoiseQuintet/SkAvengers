5be435c6ad9e49c4035c96c9a44260efde2e5540
Michael Mesereau <mmesereau1@gmail.com>
2016-08-15 15:55:17 -0600

    message

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,16 +1,37 @@
 'use strict';
 var router = require('express').Router();
+var knex = require('./db/knex')
 
 router.post('/', function(req, res, next){
+  knex('tasks').where('user_id', req.user.id)
+  .then(function(data) {
+    res.json(data);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
   //as an admin, create a new task for the current hunt
 });
 
 router.get('/:task_id', function(req, res, next){
+  knex('tasks').where('id', req.body.task_id)
+  .then(function(data) {
+    res.json(data);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
   //view a specific task
 });
 
-router.post('/user/:_task_id', function(req, res, next){
-  //submit task to be reviewed by the admin
+router.post('/user/:task_id', function(req, res, next){
+  knex('tasks').where('id', req.params.task_id)
+  .then(function(data) {
+    //submit task to be reviewed by the admin
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
 });
 
 router.post('/admin/:task_id', function(req, res, next){
@@ -19,7 +40,7 @@ router.post('/admin/:task_id', function(req, res, next){
 
 router.delete('/:task_id', function(req, res, next){
   //as an admin, delete specific task
-})
+});
 
 router.put('/:task_id', function(req, res, next){
   //as an admin, edit specific task
