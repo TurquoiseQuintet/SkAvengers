a8cca7a40488509e8b940522a2a118d0297af47b
Jamie <lanae_j@operamail.com>
2016-08-17 14:24:38 -0600

    added token stuff

--- server/app.js
+++ server/app.js
@@ -3,49 +3,60 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var logger = require('morgan');
-var cors= require('cors');
+var cors = require('cors');
 
 var root = require('./routes/auth');
 require('dotenv').config();
 
 var app = express();
-var expressJwt=require('express-jwt');
+var expressJwt = require('express-jwt');
 //decide if we need this
 // var api=require('./routes/api');
 var users = require('./routes/users');
 var hunts = require('./routes/hunts');
 var tasks = require('./routes/tasks');
-var auth = require('./routes/auth');
 var submit = require('./routes/submit');
+
 app.use(cors());
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 // app.use(function(req, res, next){
 //   console.log(req.url, req.method);
 //   next();
 // });
 app.use('/', root);
-app.use('/users', users);
-app.use('/hunts', hunts);
-app.use('/tasks', tasks);
-app.use('/submit', submit);
+// app.use('/users', users);
+// app.use('/hunts', hunts);
+// app.use('/tasks', tasks);
+// app.use('/submit', submit);
 // app.use('/', expressJwt({secret:process.env.SECRET}));
 
 // app.use('/api', expressJwt({secret:process.env.SECRET}), api);
 
-// app.use('/users', expressJwt({secret:process.env.SECRET}), users);
-// app.use('/hunts', expressJwt({secret:process.env.SECRET}), hunts);
-// app.use('/tasks', expressJwt({secret:process.env.SECRET}), tasks );
+app.use('/users', expressJwt({
+    secret: process.env.SECRET
+}), users);
+app.use('/submit', expressJwt({
+    secret: process.env.SECRET
+}), submit);
+app.use('/hunts', expressJwt({
+    secret: process.env.SECRET
+}), hunts);
+app.use('/tasks', expressJwt({
+    secret: process.env.SECRET
+}), tasks);
 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 
@@ -54,18 +65,18 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    console.log(err);
-    res.status(err.status || 500).json(err);
-  });
+    app.use(function(err, req, res, next) {
+        console.log(err);
+        res.status(err.status || 500).json(err);
+    });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
+    res.status(err.status || 500);
 });
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
-  console.log('Application is running on port:', port);
+app.listen(port, function() {
+    console.log('Application is running on port:', port);
 });
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -4,58 +4,61 @@ var knex = require('../db/knex');
 var hash = require('./hash');
 var bcrypt = require('bcrypt');
 
-function addUser(req, res, next){
-  hash(req.body.password)
-  .then(function(result) {
-    return knex('users').insert({username: req.body.username, email:req.body.email, hash: result, avatar:req.body.avatar});
-  })
-  .then(function(data){
-    res.send(data);
-  })
-  .catch(function(err){
-    res.send(err);
-  });
+function addUser(req, res, next) {
+    hash(req.body.password)
+        .then(function(result) {
+            return knex('users').insert({
+                username: req.body.username,
+                email: req.body.email,
+                hash: result,
+                avatar: req.body.avatar
+            });
+        })
+        .then(function(data) {
+            res.send(data);
+        })
+        .catch(function(err) {
+            res.send(err);
+        });
 }
 
-function checklogin (req, res, next){
+function checklogin(req, res, next) {
 
-  knex('users')
-  .where('username', req.body.username)
-  .then(function(data){
-    console.log(data[0].password);
-    if(data.length===1){
-      bcrypt.compare(req.body.password, data[0].hash, function(err, result){
+    knex('users')
+        .where('username', req.body.username)
+        .then(function(data) {
+            console.log(data[0].password);
+            if (data.length === 1) {
+                bcrypt.compare(req.body.password, data[0].hash, function(err, result) {
 
-        if(result){
-          var profile= {
-            username: data[0].username,
-            id: data[0].id,
-            email:data[0].email,
-            avatar:data[0].avatar
-        };
-        var token =jwt.sign(profile, process.env.SECRET);
-        res.status(200).json({
-          token:token
+                    if (result) {
+                        var profile = {
+                            username: data[0].username,
+                            id: data[0].id,
+                            email: data[0].email,
+                            avatar: data[0].avatar
+                        };
+                        var token = jwt.sign(profile, process.env.SECRET, {expiresIn: 432000});
+                        res.status(200).json({
+                            token: token
+                        });
+                    } else {
+                        res.json("trouble loggin in");
+                    }
+                });
+            } else {
+                console.log("err here");
+            }
+        })
+        .catch(function(err) {
+            res.json("trouble loggin in");
         });
-      }
-      else{
-        res.json("trouble loggin in");
-      }
-    });
-  }
-  else {
-    console.log("err here");
-  }
-})
-.catch(function(err){
-res.json("trouble loggin in");
-});
 }
 
 
 
 
 module.exports = {
-  addUser: addUser,
-  checklogin: checklogin
+    addUser: addUser,
+    checklogin: checklogin
 };
