e0e2f02f73d9a428dee1c22a29826edf4beb0d5a
Jamie <lanae_j@operamail.com>
2016-08-18 14:48:16 -0600

    expiration

--- routes/users.js
+++ routes/users.js
@@ -11,9 +11,9 @@ router.delete('/:user_id', userController.deleteUser);
 
 router.put('/:user_id', userController.editUser);
 
-
-<<<<<<< HEAD:server/routes/users.js
-=======
-
->>>>>>> 45bc81ff080af61dd826b8cdd66ba4bfa9323696:routes/users.js
+//
+// <<<<<<< HEAD:server/routes/users.js
+// =======
+//
+// >>>>>>> 45bc81ff080af61dd826b8cdd66ba4bfa9323696:routes/users.js
 module.exports = router;
