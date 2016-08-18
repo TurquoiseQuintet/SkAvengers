3944ff83a687ba5af9b7460785e3094e12b6cb3a
Michael Mesereau <mmesereau1@gmail.com>
2016-08-18 17:03:49 -0600

    clls

--- controllers/submitController.js
+++ controllers/submitController.js
@@ -10,29 +10,36 @@ function submit(req, res) {
   console.log("begin");
   knex('users').where('username', req.params.user_id)
   .then(function(data) {
+    console.log("call 1");
     user = data[0];
     return knex('users_tasks').where('users_id', req.params.user_id).where('tasks_id', req.params.task_id);
   })
   .then(function(data) {
+    console.log("call 2");
     user_task = data[0];
     return knex('tasks').where('id', user_task.id);
   })
   .then(function(data) {
+    console.log("call 3");
     task = data[0];
     return knex('users_tasks').where('users_id', req.params.user_id).where('tasks_id', task.id).update({completed: true});
   })
   .then(function() {
+    console.log("call 4");
     return knex('hunts').where('id', task.hunt_id);
   })
   .then(function(data) {
+    console.log("call 5");
     hunt = data[0];
     return knex('hunts_users').where('hunts_id', hunt.id).where('users_id', req.params.user_id);
   })
   .then(function(data) {
+    console.log("call 6");
     hunt_user = data[0];
     return knex('hunts_users').where('hunts_id', hunt.id).where('users_id', req.params.user_id).update({experience: (parseInt(data[0].experience) + parseInt(task.xp))});
   })
   .then(function() {
+    console.log("call 7");
     if (task.unique) {
       return knex('users_tasks').where('tasks_id', task.id).update('completed', true);
     }
