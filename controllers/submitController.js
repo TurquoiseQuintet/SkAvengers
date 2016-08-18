26148584db21b171bb68de63b7f68b9e9ed795e3
Michael Mesereau <mmesereau1@gmail.com>
2016-08-18 17:01:10 -0600

    submit

--- controllers/submitController.js
+++ controllers/submitController.js
@@ -30,7 +30,7 @@ function submit(req, res) {
   })
   .then(function(data) {
     hunt_user = data[0];
-    return knex('hunts_users').where('hunts_id', hunt.id).where('users_id', req.params.user_id).update({experience: (parseInt(hunt_user.experience) + parseInt(task.xp))});
+    return knex('hunts_users').where('hunts_id', hunt.id).where('users_id', req.params.user_id).update({experience: (parseInt(data[0].experience) + parseInt(task.xp))});
   })
   .then(function() {
     if (task.unique) {
